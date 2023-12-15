e627a39a60a857cec9910ad36e17183759925499
craeyefish <craeyecian@gmail.com>
2023-12-15 10:52:03 +0200

    cleanup

--- src/app/q15/q15.component.ts
+++ src/app/q15/q15.component.ts
@@ -57,40 +57,30 @@ export class Q15Component implements OnInit {
     let boxLenses = new Map<number, Lens[]>();
 
     for (var i = 0; i < inputs.length; i++) {
-      let val = 0;
-
       let boxStr = inputs[i].includes('=')
         ? inputs[i].split('=')[0]
         : inputs[i].split('-')[0];
       let boxNum = 0;
-      // console.log(boxStr);
       for (var j = 0; j < boxStr.length; j++) {
         boxNum += boxStr[j].charCodeAt(0);
         boxNum = boxNum * 17;
         boxNum = boxNum % 256;
       }
-      // console.log(boxNum);
-
-      // total += val;
 
       let inputLens = inputs[i].includes('=')
         ? parseInt(inputs[i].split('=')[1])
         : parseInt(inputs[i].split('-')[1]);
-      // console.log('InputLens: ' + inputLens);
       let lenses = boxLenses.get(boxNum);
 
       if (inputs[i].includes('-') && lenses != undefined) {
-        // console.log('- found');
         let index = -1;
         for (var j = 0; j < lenses.length; j++) {
-          // console.log(lenses[j]);
           if (lenses[j].label == boxStr) {
             index = j;
           }
         }
 
         if (index != -1) {
-          // console.log('removing ' + index + ' ' + boxStr + ' from ' + boxNum);
           lenses.splice(index, 1);
         }
 
@@ -98,9 +88,7 @@ export class Q15Component implements OnInit {
       }
 
       if (inputs[i].includes('=')) {
-        // console.log('= found');
         if (lenses == undefined) {
-          // console.log('adding first item to box: ' + boxStr + ' ' + inputLens);
           boxLenses.set(boxNum, [{ label: boxStr, val: inputLens }]);
         } else {
           let found = false;
@@ -111,15 +99,10 @@ export class Q15Component implements OnInit {
             }
           }
           if (!found) {
-            // console.log(lenses);
-            // console.log('pushing lens');
             lenses.push({ label: boxStr, val: inputLens });
-            // console.log(lenses);
           }
         }
       }
-
-      // console.log(boxLenses);
     }
 
     boxLenses.forEach((val, key) => {
